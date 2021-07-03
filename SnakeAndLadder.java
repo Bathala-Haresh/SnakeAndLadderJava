8d8313b71e8bf35a0526fe163897e3475f3f2879
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-03 08:41:48 +0530

    welcome to snakes and ladder

---
+++ SnakeAndLadder.java
@@ -0,0 +1,16 @@
+/**
+* This program is to welcome to snake and ladder
+*
+* @author  Bathala Haresh
+*/
+
+public class SnakeAndLadder
+{
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Welcome to snake and ladder problem ");
+		
+	}
+}
